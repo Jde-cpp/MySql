60acd8870729c03af3e99a30c6f16de60ece00f4
jde-cpp <johnduffyx@outlook.com>
2024-01-01 16:27:40 -0500

    endl

--- source/MySqlStatements.cpp
+++ source/MySqlStatements.cpp
@@ -2,17 +2,18 @@
 
 namespace Jde::DB::MySql::Sql
 {
+	using std::endl;
 	string ColumnSql( bool addTable )noexcept
 	{
 		std::ostringstream os;
-		os << "select	t.TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION, COLUMN_DEFAULT, IS_NULLABLE, COLUMN_TYPE, CHARACTER_MAXIMUM_LENGTH, EXTRA='auto_increment' is_identity, 0 is_id, NUMERIC_PRECISION, NUMERIC_SCALE" << std::endl
-			<< "from		INFORMATION_SCHEMA.TABLES t" << std::endl
-			<< "\tinner join INFORMATION_SCHEMA.COLUMNS c on t.TABLE_CATALOG=c.TABLE_CATALOG and t.TABLE_SCHEMA=c.TABLE_SCHEMA and t.TABLE_NAME=c.TABLE_NAME" << std::endl
-			<< "\twhere t.TABLE_SCHEMA=?" << std::endl
-			<< "\tand\tt.TABLE_TYPE='BASE TABLE'" << std::endl;
+		os << "select	t.TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION, COLUMN_DEFAULT, IS_NULLABLE, COLUMN_TYPE, CHARACTER_MAXIMUM_LENGTH, EXTRA='auto_increment' is_identity, 0 is_id, NUMERIC_PRECISION, NUMERIC_SCALE" << endl
+			<< "from		INFORMATION_SCHEMA.TABLES t" << endl
+			<< "\tinner join INFORMATION_SCHEMA.COLUMNS c on t.TABLE_CATALOG=c.TABLE_CATALOG and t.TABLE_SCHEMA=c.TABLE_SCHEMA and t.TABLE_NAME=c.TABLE_NAME" << endl
+			<< "\twhere t.TABLE_SCHEMA=?" << endl
+			<< "\tand\tt.TABLE_TYPE='BASE TABLE'" << endl;
 		if( addTable )
-			os << "\tand t.TABLE_NAME=:table_name" << std::endl;
-		os << "order by t.TABLE_NAME, ORDINAL_POSITION" << std::endl;
+			os << "\tand t.TABLE_NAME=:table_name" << endl;
+		os << "order by t.TABLE_NAME, ORDINAL_POSITION" << endl;
 		return os.str();
 	}
 
